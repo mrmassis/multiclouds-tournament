8c17669c6d555a9b1f3971b659af2ee526965bb6
Marcio Roberto Miranda Assis <marcio-roberto.assis@serpro.gov.br>
2016-05-09 16:04:03 -0300

    add git ignore and insert the division.py file

--- MCT_Referee/lib/division.py
+++ MCT_Referee/lib/division.py
@@ -13,8 +13,6 @@ import logging.handlers;
 
 from lib.database     import Database;
 from multiprocessing  import Process, Queue, Lock;
-from lib.scheduller   import Roundrobin;
-from lib.amqp         import RabbitMQ_Publish, RabbitMQ_Consume;
 
 
 
@@ -65,39 +63,39 @@ logger.addHandler(handler);
 ###############################################################################
 ## CLASSES                                                                   ##
 ###############################################################################
-#class Division(Process):
+class Division(Process):
 
-#    """
-#    Class Division:
-#    ---------------------------------------------------------------------------
-#    PUBLIC METHODS:
-#    * run == division main loop.
-#    """
+    """
+    Class Division:
+    ---------------------------------------------------------------------------
+    PUBLIC METHODS:
+    * run == division main loop.
+    """
 
     ###########################################################################
     ## ATTRIBUTES                                                            ##
     ###########################################################################
-#    __dNumb = None;
-#    __dName = None;
-#    __dConf = None;
-#    __dBase = None;
-#    __dLock = None;
+    __dNumb = None;
+    __dName = None;
+    __dConf = None;
+    __dBase = None;
+    __dLock = None;
 
 
     ###########################################################################
     ## SPECIAL METHODS                                                       ##
     ###########################################################################
-#    def __init__(self, dNumb, dName, dConf, dBase, dLock):
-#        super(Division, self).__init__(name=dName);
+    def __init__(self, dNumb, dName, dConf, dBase, dLock):
+        super(Division, self).__init__(name=dName);
 
-#        self.__dNumb = dNumb;
-#        self.__dName = dName;
-#        self.__dConf = dConf;
-#        self.__dBase = dBase;
-#        self.__dLock = dLock;
+        self.__dNumb = dNumb;
+        self.__dName = dName;
+        self.__dConf = dConf;
+        self.__dBase = dBase;
+        self.__dLock = dLock;
 
         ## LOG:
-#        logger.info('STARTED DIVISION %s', dName);
+        logger.info('STARTED DIVISION %s', dName);
 
 
     ###########################################################################
@@ -237,347 +235,5 @@ logger.addHandler(handler);
         ## LOG:
         logger.info("CALCULATE HISTC");
         return oldHistc;
-## END.
-
-
-
-
-
-
-class MCT_Referee(RabbitMQ_Consume):
-
-    """
-    Class MCT_Referee: start and mantain the MCT referee.
-    ---------------------------------------------------------------------------
-    PUBLIC METHODS:
-    ** run            == create the divisions and wait in loop.
-    ** gracefull_stop == grecefull finish the divisions.
-    """
-
-    ###########################################################################
-    ## ATTRIBUTES                                                            ##
-    ###########################################################################
-    __threadsId     = None;
-    __allQueues     = None;
-    __routeDispatch = None;
-    __dbConnection  = None;
-    __scheduller    = None;
-
-
-    ###########################################################################
-    ## SPECIAL METHODS                                                       ##
-    ###########################################################################
-    def __init__(self):
-
-        ## Get the configurations related to the execution of the divisions de
-        ## fined by the User.
-        configs = self.__get_configs(CONFIG_FILE);
-
-        ## Get which route is used to deliver the message to the MCT_Dispatch.
-        self.__routeDispatch = configs['amqp_publish']['route'];
-
-        ## Initialize the inherited class RabbitMQ_Consume with the parameters
-        ## defined in the configuration file.
-        RabbitMQ_Consume.__init__(self, configs['amqp_consume']);
-
-        ## Instantiates an object to perform the publication of AMQP messages.
-        self.__publish=RabbitMQ_Publish(configs['amqp_publish']);
-
-        ## Intance a new object to handler all operation in the local database
-        self.__db = Database(configs['database']);
-
-        ## Select the scheduller algorithm responsible for selection of the be-
-        ## st player in a division.
-        #if configs['scheduller']['approach'] == 'roundrobin':
-        #    self.__scheduller = Roundrobin(configs['scheduller']['restrict']);
-
-        ## This list store the thread IDs that represet each division started.
-        #self.__threadsId = [];
-
-        ##
-        #self.__lock = Lock();
-
-        ## Instance the divisions that will be used in MCT:
-        #for divNumb in range(1, int(configs['main']['num_divisions']) + 1):
-        #     divName = 'division' + str(divNumb);
-        #     divConf = configs[divName];
-
-        #     division=Division(divNumb,divName,divConf,self.__db,self.__lock);
-        #     division.daemon = True;
-        #     division.start();
-
-        #     self.__threadsId.append(division);
-
-
-    ###########################################################################
-    ## PUBLIC METHODS                                                        ##
-    ###########################################################################
-    ##
-    ## BRIEF: grecefull finish the divisions.
-    ## ------------------------------------------------------------------------
-    ##
-    def gracefull_stop(self):
-        for thread in self.__threadsId:
-            thread.terminate();
-            thread.join();
-
-        ## LOG:
-        logger.info('GRACEFULL STOP ...');
-        return 0;
-
-
-    ##
-    ## BRIEF: method invoked when the pika receive a message.
-    ## ------------------------------------------------------------------------
-    ## @PARAM pika.Channel              channel    = the communication channel.
-    ## @PARAM pika.spec.Basic.Deliver   method     = 
-    ## @PARAM pika.spec.BasicProperties properties = 
-    ## @PARAM str                       message    = message received.
-    ##
-    def callback(self, channel, method, properties, message):
-
-        ## LOG:
-        logger.info('MESSAGE %s RECEIVED FROM: %s.',message,properties.app_id);
-
-        ## Send to source an ack msg to ensuring that the message was received.
-        self.chn.basic_ack(method.delivery_tag);
-
-        ## The json.loads translate a string containing JSON data into a Python
-        ## dictionary format.
-        message = json.loads(message);
-
-        ## Check the messsge received.Verify if all fields are presents and are
-        ## in correct form.
-        valRet = self.__inspect_request(message);
-
-        if valRet == 0:
-
-            ## Check if is a request or a response received from the apropriate
-            ## player. When message['retId'] equal a '' the msg is a request.
-            if message['retId'] == '':
-
-                ## Get which division than player belong.It is necessary to get
-                ## the player list able to meet the request.
-                division = self.__get_division(message['playerId']);
-
-                ## --------------------------------------------------------- ##
-                ## [0] == GET RESOUCES INF.                                  ##
-                ## --------------------------------------------------------- ##
-                if   int(message['code']) == 0:
-
-                    ## Get all resources available to a division. Check in db.
-                    message['data'] = self.__get_resources_inf(division);
-
-                ## --------------------------------------------------------- ##
-                ## [1] CREATE A NEW INSTANCE.                                ##
-                ## --------------------------------------------------------- ##
-                elif int(message['code']) == 1:
-                    message = self.__add_instance(division, message);
-
-                ## --------------------------------------------------------- ##
-                ## [2] DELETE AN INSTANCE.                                   ##
-                ## --------------------------------------------------------- ##
-                elif int(message['code']) == 2:
-                    message = self.__del_instance(division, message);
-    
-            else:
-                ## Set the field to forward value:
-                message['retId'] = '';
-
-            self.__publish.publish(message, self.__routeDispatch);
-        return 0;
-
-
-    ###########################################################################
-    ## PRIVATE METHODS                                                       ##
-    ###########################################################################
-    ##
-    ## BRIEF: check if reques is valid.
-    ## ------------------------------------------------------------------------
-    ## @PARAM dict request == received request.
-    ##
-    def __inspect_request(self, request):
-        ## LOG:
-        logger.info('INSPECT REQUEST!');
-
-        key = request['code'];
-
-        query = "SELECT fields FROM FIELDS WHERE operation='" + str(key) + "'";
-
-        ##self.__lock.acquire();
-        #valRet = [] or self.__db.select_query(query);
-        ##self.__lock.release();
-
-        #if valRet != []:
-        #    fields = valRet[0][0];
-
-        #     for field in fields:
-        #         if not request.has_key(field):
-        #             print "LOG: missed field " + key;
-        #             return 1;
-
-        #    return 0;
-
-        return 0;
-
-
-    ##
-    ## BRIEF: get the player's division.
-    ## ------------------------------------------------------------------------
-    ## @PARAM str playerId == player identifier.
-    ## TODO: testar.
-    ##
-    def __get_division(self, playerId):
-        ## LOG:
-        logger.info('INSPECT REQUEST!');
-
-        ## Setting 
-        division = -1;
-
-        query = "SELECT division FROM PLAYER WHERE name='" + playerId + "'";
-
-        self.__lock.acquire();
-        valRet = [] or self.__db.select_query(query);
-        self.__lock.release();
-
-        if valRet != []:
-            division = valRet[0][0];
-
-        return division;
-
-
-    ##
-    ## BRIEF: create a new instance.
-    ## ------------------------------------------------------------------------
-    ## @PARAM int division ==.
-    ## @PARAM dict message ==.
-    ##
-    def __add_instance_inf(self, division, message):
-        ##.
-        message['retId'] = message['reqId'];
-
-        ## Select one player able to comply a request to create VM.
-        message['destAdd'] = self.__get_player(division, message['playerId']);
-
-        ## TODO: se for vazio o message entao retorna erro e retorna para o re-
-        ##       quisitante.
-
-        return message;
- 
-
-    ##
-    ## BRIEF: delete an instance.
-    ## ------------------------------------------------------------------------
-    ## @PARAM int division ==.
-    ## @PARAM dict message ==.
-    ##
-    def __del_instance_inf(self, division, message):
-        return {};
-
-
-    ##
-    ## BRIEF: get the resources info to specfic division.
-    ## ------------------------------------------------------------------------
-    ## @PARAM int division.
-    ##
-    def __get_resources_inf(self, division):
-        resouces = {};
-
-        ## Mount the database query: 
-        query  = "SELECT ";
-        query += "vcpu, memory, disk, vcpu_used, memory_used, disk_used ";
-        query += "FROM RESOURCE WHERE ";
-        query += "division='" + str(division) + "'";
-
-        self.__acquire();
-        valRet = [] or self.__db.select_query(query);
-        self.__release();
-
-        if valRet != []:
-
-            resources = {
-                'vcpu'          : valRet[0][0],
-                'memory_mb'     : valRet[0][1],
-                'disk_mb'       : valRet[0][2],
-                'vcpu_used'     : valRet[0][3],
-                'memory_mb_used': valRet[0][4],
-                'disk_mb_used'  : valRet[0][5]
-            }
-
-        return resources;
-
-
-    ##
-    ## BRIEF: choice the best player to perform the request.
-    ## ------------------------------------------------------------------------
-    ## @PARAM int division.
-    ## TODO: implementar o scheduler, entrada uma lista de players.
-    ##
-    ##
-    def __get_choice_player(self, division, playerIdRequest):
-
-       playerAddress = '';
-
-       ## Genereate the query to select the players from specific division.
-       query = "SELECT * FROM PLAYER WHERE division='" + division + "'";
-       
-       self.__acquire();
-       valRet = [] or self.__db.select_query(query);
-       self.__release();
-
-       if valRet != []:
-           ## Perform the player selection.
-           playerAddress = self.__scheduller.run(valRet[0], playerIdRequest);
-
-       ## 
-       return playerAddress;
-
-
-    ##
-    ## BRIEF: get config options.
-    ## ------------------------------------------------------------------------
-    ## @PARAM str configName == file with configuration.
-    ##
-    def __get_configs(self, configName):
-        cfg = {};
-
-        config = ConfigParser.ConfigParser();
-        config.readfp(open(configName));
-
-        ## Scan the configuration file and get the relevant informations and sa
-        ## ve then in cfg dictionary.
-        for section in config.sections():
-            cfg[section] = {};
-
-            for option in config.options(section):
-                cfg[section][option] = config.get(section,option);
-
-        return cfg;
-## END.
-
-
-
-
-
-
-
-
-###############################################################################
-## MAIN                                                                      ##
-###############################################################################
-if __name__ == "__main__":
-    ## LOG:
-    logger.info('EXECUTION STARTED...');
-
-    try:
-        mctReferee = MCT_Referee();
-        mctReferee.consume();
-
-    except KeyboardInterrupt, error:
-        mctReferee.gracefull_stop();
-
-    ## LOG:
-    logger.info('EXECUTION FINISHED...');
-    sys.exit(0);
 ## EOF.
 
