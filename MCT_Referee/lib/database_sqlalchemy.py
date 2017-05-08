337ab7cb67ed7a230930b2a1d9fb07c632455ade
mrmassis <marcio.miranda.assis@gmail.com>
2017-05-08 15:23:26 -0300

    separate libs to merge other day

---
+++ MCT_Referee/lib/database_sqlalchemy.py
@@ -0,0 +1,363 @@
+#!/usr/bin/python
+
+
+
+
+
+
+
+
+__all__ = ['MCT_Database_SQLAlchemy', 'Simulation', 'State', 'Request', 'Player', 'Fairness']
+
+
+
+
+
+
+
+
+###############################################################################
+## IMPORT                                                                    ##
+###############################################################################
+from sqlalchemy                 import *;
+from sqlalchemy.ext.declarative import declarative_base;
+from sqlalchemy.orm             import relation, sessionmaker, mapper;
+from sqlalchemy.dialects.mysql  import *;
+
+
+
+
+
+
+
+###############################################################################
+## DEFINITION                                                                ##
+###############################################################################
+Base = declarative_base();
+
+
+
+
+
+
+
+###############################################################################
+## CLASSES                                                                   ##
+###############################################################################
+class Simulation(Base):
+
+    """
+    Class Simulation:
+    ---------------------------------------------------------------------------
+    """
+
+    ###########################################################################
+    ## ATTRIBUTES                                                            ##
+    ###########################################################################
+    __tablename__ = 'SIMULATION';
+
+    id          = Column(BIGINT(20) , nullable=False, primary_key=True); 
+    time        = Column(BIGINT(20) , nullable=False);
+    machineId   = Column(BIGINT(20) , nullable=False);
+    eventType   = Column(INT(11)    , nullable=False);
+    plataformId = Column(VARCHAR(45), nullable=True );
+    cpu         = Column(FLOAT      , nullable=False);
+    memory      = Column(FLOAT      , nullable=False);
+    valid       = Column(INT(11)    , default=0);
+        
+    id.autoincrement = True;
+
+## END CLASS.
+
+
+
+
+
+
+
+
+class State(Base):
+
+    """
+    Class State:
+    ---------------------------------------------------------------------------
+    """
+
+    ###########################################################################
+    ## ATTRIBUTES                                                            ##
+    ###########################################################################
+    __tablename__ = 'STATE';
+
+    player_id = Column(VARCHAR(45), nullable=False, primary_key=True); 
+    vm_id     = Column(VARCHAR(45), nullable=False);
+    vm_owner  = Column(VARCHAR(45), nullable=False);
+    vm_type   = Column(VARCHAR(45), nullable=False);
+    running   = Column(VARCHAR(45), nullable=False);
+
+## END CLASS.
+    
+
+
+
+
+
+
+
+class Request(Base):
+
+    """
+    Class Request:
+    ---------------------------------------------------------------------------
+    """
+
+    ###########################################################################
+    ## ATTRIBUTES                                                            ##
+    ###########################################################################
+    __tablename__ = 'REQUEST';
+
+    id                 = Column(INT        , nullable=False, primary_key=True);
+    player_id          = Column(VARCHAR(45), nullable=False);
+    request_id         = Column(VARCHAR(45), nullable=False);
+    message            = Column(LONGTEXT   , nullable=False);
+    action             = Column(INT        , nullable=True);
+    timestamp_received = Column(TIMESTAMP  , nullable=True);
+    timestamp_finished = Column(TIMESTAMP  , nullable=True);
+    status             = Column(TINYINT(1) , nullable=True);
+
+## END CLASS.
+
+    
+
+
+
+
+
+
+
+class Fairness:
+
+    """
+    Class Fairness:
+    ---------------------------------------------------------------------------
+    """
+
+    ###########################################################################
+    ## ATTRIBUTES                                                            ##
+    ###########################################################################
+    __tablename__ = 'FAIRNESS';
+## END CLASS.
+
+
+
+
+
+
+
+
+class Player(Base):
+
+    """
+    Class Player:
+    ---------------------------------------------------------------------------
+    """
+
+    ###########################################################################
+    ## ATTRIBUTES                                                            ##
+    ###########################################################################
+    __tablename__ = 'PLAYER';
+
+    player_id      = Column(VARCHAR(45), nullable=False, primary_key=True); 
+    vcpus          = Column(INT        , nullable=True , default=0); 
+    vcpus_used     = Column(INT        , nullable=True , default=0); 
+    local_gb       = Column(INT        , nullable=True , default=0);
+    local_gb_used  = Column(INT        , nullable=True , default=0);
+    memory         = Column(INT        , nullable=True , default=0);
+    memory_mb_used = Column(INT        , nullable=True , default=0);
+    requests       = Column(INT        , nullable=True , default=0);
+    accepted       = Column(INT        , nullable=True , default=0);
+    fairness       = Column(FLOAT      , nullable=True , default=0);
+
+## END CLASS.
+
+
+
+
+
+
+
+
+
+class MCT_Database_SQLAlchemy:
+
+    """
+    Class MCT_Database_SQLAlchemy:
+    ---------------------------------------------------------------------------
+    """
+
+    ###########################################################################
+    ## ATTRIBUTES                                                            ##
+    ###########################################################################
+    session = None;
+
+
+    ###########################################################################
+    ## SPECIAL METHODS                                                       ##
+    ###########################################################################
+    ##
+    ## BRIEF: initialize the object.
+    ## ------------------------------------------------------------------------
+    ##
+    def __init__(self, dbDictionary):
+
+        parameter  = 'mysql://';
+        parameter += dbDictionary['user'];
+        parameter += ':';
+        parameter += dbDictionary['pass'];
+        parameter += '@';
+        parameter += dbDictionary['host'];
+        parameter += '/';
+        parameter += dbDictionary['base'];
+
+        ##
+        engine = create_engine(parameter);
+
+        ##
+        DBSession = sessionmaker(bind=engine)
+
+        ## A DBSession() instance establishes all conversations with the databa
+        ## se and represents a staging zone for all the objects loaded into the
+        ## database session object. Any change made against the objects in the 
+        ## session won't be persisted into the database until you call session.
+        ## commit(). If you are not happy about the changes, you can revert all
+        ## of them back to the last commit by calling session.rollback();
+        self.session = DBSession();
+
+
+    ###########################################################################
+    ## PUBLIC METHODS                                                        ##
+    ###########################################################################
+    ##
+    ## BRIEF: return all registries
+    ## ------------------------------------------------------------------------
+    ## @PARAM obj table == object that meaning one table in database.
+    ##
+    def all_regs(self, table):
+        rList = [];
+
+        ## Obtain all registries from database:
+        allRegistries = self.session.query(table).all();
+
+        for registry in allRegistries:
+            rList.append(self.__row2dict(registry));
+
+        return rList;
+
+
+    ##
+    ## BRIEF: return first registry
+    ## ------------------------------------------------------------------------
+    ## @PARAM obj table == object that meaning one table in database.
+    ##
+    def first_reg(self, table):
+        rList = [];
+
+        ## Obtain the first registry from database:
+        registry = self.session.query(table).first();
+
+        rList.append(self.__row2dict(registry));
+
+        return rList;
+
+
+    ##
+    ## BRIEF: return all registries from database (with filter).
+    ## ------------------------------------------------------------------------
+    ## @PARAM obj table      == object that meaning one table in database.
+    ## @PARAM dct filterRules== filters to apply.
+    ##
+    def all_regs_filter(self, table, filterRule):
+        rList = [];
+
+        ## Obtain all registries from database:
+        allRegistries = self.session.query(table).filter(filterRule).all();
+
+        for registry in allRegistries:
+            rList.append(self.__row2dict(registry));
+
+        return rList;
+
+
+    ##
+    ## BRIEF: return the first registry from database (with filter).
+    ## ------------------------------------------------------------------------
+    ## @PARAM obj table      == object that meaning one table in database.
+    ## @PARAM dct filterRules== filters to apply.
+    ##
+    def first_reg_filter(self, table, filterRules):
+
+        rList = [];
+
+        ## Execute the query:
+        querySql = self.session.query(table)
+
+        ## Filter:
+        for attr, value in filterRules.items():
+            if querySql:
+                querySql = querySql.filter(value);
+
+        ## Get the first element found in the table:
+        registry = querySql.first();
+
+        ## Convert the object structure to a pythoninc dictionary format:
+        if registry:
+            rList.append(self.__row2dict(registry));
+
+        return rList;
+
+
+    ##
+    ## BRIEF: update a registry.
+    ## ------------------------------------------------------------------------
+    ## @PARAM obj table      == object that meaning one table in database.
+    ## @PARAM str filterRule == filter to apply.
+    ## @PARAM dic fieldsDict == dictionary with fields:value to update.
+    ##
+    def update_reg(self, table, filterRule, fieldsDict):
+
+        ## Update the registry:
+        self.session.query(table).filter(filterRule).update(fieldsDict);
+        self.session.commit();
+
+        return 0;
+
+
+    ##
+    ## BRIEF: insert a new register in table;
+    ## ------------------------------------------------------------------------
+    ## @PARAM obj table      == object that meaning one table in database.
+    ## @PARAM dic fieldsDict == dictionary with fields:value to update.
+    ##
+    def insert_reg(self, row):
+
+        self.session.add(row);
+        self.session.commit();
+
+        return 0;
+
+
+    ##
+    ## BRIEF: convert the sqlalchemy row to python dictionary.
+    ## ------------------------------------------------------------------------
+    ## @PARAM row == row to convert. 
+    ## 
+    def __row2dict(self, row):
+        dictionary = {};
+
+        ##
+        for column in row.__table__.columns:
+            dictionary[column.name] = str(getattr(row, column.name));
+
+        return dictionary
+## END CLASS.
+
+## END.
---
+++ MCT_Referee/lib/utils.py
@@ -0,0 +1,215 @@
+#!/usr/bin/python
+
+
+
+
+###############################################################################
+## IMPORT                                                                    ##
+###############################################################################
+import ConfigParser;
+import inspect;
+import random;
+import time;
+
+
+
+
+
+###############################################################################
+## DEFINITION                                                                ##
+###############################################################################
+## Value defined to status when dont find some fields.
+MESSAGE_PARSE_ERROR = -10;
+
+## Invalid division:
+DIVISION_INVALID = -1;
+
+## Operation code:
+GETINF_RESOURCE = 0
+SETINF_RESOURCE = 8
+CREATE_INSTANCE = 1
+DELETE_INSTANCE = 2
+SUSPND_INSTANCE = 3
+RESUME_INSTANCE = 4
+RESETT_INSTANCE = 5
+GETINF_INSTANCE = 9
+
+## Instances definitions (SIMULATION):
+IMG_NAME = 'cirros-0.3.3-x86_64';
+NET_NAME = 'demo-net';
+
+## Information about the flavors. Get from openstack flavors!
+FLV_NAME = {'T':'m1.tiny', 'S':'m1.small', 'B':'m1.medium'};
+CPU_INFO = {'T':'1'  , 'S':'1'   , 'B':'2'   };
+MEM_INFO = {'T':'512', 'S':'2048', 'B':'4096'};
+DSK_INFO = {'T':'1'  , 'S':'20'  , 'B':'40'  };
+
+## Dictionary with the convertion of return signal.
+GENERIC_STATUS = {
+    CREATE_INSTANCE : { 'NOSTATE':0, 'ERROR':0, 'ACTIVE':1},
+    DELETE_INSTANCE : { 'NOSTATE':0, 'ERROR':0, 'HARD_DELETED':1,'DELETED':1},
+    SUSPND_INSTANCE : { 'NOSTATE':0, 'ERROR':0, 'SUSPENDED'   :1},
+    RESUME_INSTANCE : { 'NOSTATE':0, 'ERROR':0, 'ACTIVE'      :1}
+}
+
+
+
+
+
+###############################################################################
+## PROCEDURES                                                                ##
+###############################################################################
+##
+## BRIEF: get config options.
+## ----------------------------------------------------------------------------
+## @PARAM str configName == file with configuration.
+##
+def get_configs(configName):
+    cfg = {};
+
+    try:
+        config = ConfigParser.ConfigParser();
+        config.readfp(open(configName));
+
+        ## Scan the configuration file and get the relevant infos. Save infos
+        ## in a dictionary.
+        for section in config.sections():
+            cfg[section] = {};
+
+            for option in config.options(section):
+                cfg[section][option] = config.get(section,option);
+
+        return cfg;
+
+    except ConfigParser.Error as cfgError:
+        raise cfgError;
+
+    return {};
+
+
+
+
+##
+## BRIEF: get the caller class.
+## ----------------------------------------------------------------------------
+## @PARAM deep == deep from get information.
+##
+def get_class_name_from_frame(deep=0):
+    stack = inspect.stack();
+
+    frame = stack[deep][0];
+    args, _, _, value_dict = inspect.getargvalues(frame);
+
+    string = value_dict['__file__'];
+
+    ## Extract the information from the name of file.
+    string = string.replace('./' ,'');
+    string = string.replace('.py','');
+
+    return string;
+
+
+##
+## BRIEF: waiting for a time select from a interval.
+## ----------------------------------------------------------------------------
+## @PARAM ini == start the interval to select. 
+## @PARAM end == finish the interval.
+##
+def mutable_time_to_waiting(ini, end):
+
+    value = random.uniform(ini, end)
+    time.sleep(value);
+
+
+
+
+###############################################################################
+## CLASSES                                                                   ##
+###############################################################################
+class Show_Actions:
+
+    """
+    Show_Actions: class that print the received message in the screen or log.
+    ---------------------------------------------------------------------------
+    """
+
+    ###########################################################################
+    ## ATTRIBUTES                                                            ##
+    ###########################################################################
+    __choice = None;
+    __logger = None;
+
+
+    ###########################################################################
+    ## SPECIAL METHODS                                                       ##
+    ###########################################################################
+    def __init__(self, choice, logger=None):
+
+        ## Choice the method to print the message.
+        self.__choice = choice;
+
+        ## Assing the logger object to the local attribute. It enable choice to
+        ## where the messagem will be printed.
+        self.__logger = logger;
+
+
+    ###########################################################################
+    ## PUBLIC METHOS                                                         ##
+    ###########################################################################
+    ##
+    ## BRIEF: show to screen or log the message.
+    ## ------------------------------------------------------------------------
+    ## @PARAM str msg      == message to show (screen or logger).
+    ## @PARAM str severity == message severity.
+    ##
+    def show(self, message, severity):
+
+        if   self.__choice == 'screen':
+            self.__showScreen(message, severity);
+
+        elif self.__choice == 'logger':
+            self.__showLogger(message, severity);
+
+        else:
+            self.__showBothSL(message, severity);
+
+
+
+    ###########################################################################
+    ## PRIVATE                                                              ##
+    ###########################################################################
+    ##
+    ## BRIEF: show message on the screen and logger.
+    ## ------------------------------------------------------------------------
+    ## @PARAM str msg      == message to show (screen or logger).
+    ## @PARAM str severity == message severity.
+    ##
+    def __showBothSL(self, message, severity):
+        self.__showScreen(message, severity);
+        self.__showLogger(message, severity);
+
+
+    ##
+    ## BRIEF: show message on the screen.
+    ## ------------------------------------------------------------------------
+    ## @PARAM str msg      == message to show (screen or logger).
+    ## @PARAM str severity == message severity.
+    ##
+    def __showScreen(self, message, severity):
+        print '[' + severity + '] ' + message;
+
+
+    ##
+    ## BRIEF: show message in the logger.
+    ## ------------------------------------------------------------------------
+    ## @PARAM str msg      == message to show (screen or logger).
+    ## @PARAM str severity == message severity.
+    ##
+    def __showLogger(self, message, severity):
+
+        if   severity == 'I':
+            self.__logger.info(message);
+
+        elif severity == 'E':
+            self.__logger.error(message);
+## EOF.
